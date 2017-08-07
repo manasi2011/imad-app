5ff8131df4bf03ca39e50987ed56fac021ed1e1c
manasi2011 <30698065+manasi2011@users.noreply.github.com>
2017-08-07 12:10:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/artical-one',function(req, res){
+app.get('/articale-one',function(req, res){
     res.send('Artical one requested and served here');
 });
 
